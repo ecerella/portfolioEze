c8756acd2f2deeda13b741b20ca3c93fe4f1e9c1
ecerella <ecerella@gmail.com>
2023-01-05 14:20:28 -0300

    agrego educacion, copiando experiencia, queda funcional back y front

---
+++ back/eze/src/main/java/com/portfolio/eze/Repository/REducacion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.eze.Repository;
+
+import com.portfolio.eze.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ezequiel
+ */
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
